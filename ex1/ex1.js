daf30bd8b6298d44f725101d6edbf6f0e6966cad
Thanakorn Sriwannawit <64070040@kmitl.ac.th>
2023-02-16 22:53:37 +0700

    update

--- ex1/ex1.js
+++ ex1/ex1.js
@@ -1,2 +1,10 @@
+
+var student_info = {
+        student_id: "64070040",
+        student_fname: "ธนกร",
+        student_lname: "ศรีวรรณวิทย์"
+    }
 var app = new Vue({
+    el: "#ex1",
+    data: student_info
 })
